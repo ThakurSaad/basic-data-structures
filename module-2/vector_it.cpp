a472da83bee15b139b303619368f6768f8178d4e
ThakurSaad <thakursaad613@gmail.com>
2025-06-18 16:43:55 +0600

    vector iterator

---
+++ module-2/vector_it.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6, 76, 87, 8, 56, 4, 3};
+
+    for (auto it = v.begin(); it < v.end(); it++)
+    {
+        cout << *it << endl;
+    }
+
+    return 0;
+}
