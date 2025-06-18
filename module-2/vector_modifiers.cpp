fd9bd7a068bbd8a758cb08a4e48be2e19bac283c
ThakurSaad <thakursaad613@gmail.com>
2025-06-18 16:34:49 +0600

    vector modifiers

--- module-2/vector_modifiers.cpp
+++ module-2/vector_modifiers.cpp
@@ -8,7 +8,7 @@ int main()
 
     // vector<int> v2;
     // v.assign();
-    v2.insert(v2.begin() + 1, {200, 300, 400, 5600});
+    v2.insert(v2.begin() + 1, {200, 300, 400, 5600, 31, 400, 31, 20, 400});
 
     // for (int i = 0; i < v2.size(); i++)
     // {
@@ -16,12 +16,25 @@ int main()
     // }
 
     // v2.erase(v2.begin() + 1, v2.begin() + 4);
-    v2.erase(v2.begin() + 3, v2.end());
+    // v2.erase(v2.begin() + 3, v2.end());
+    // replace(v2.begin(), v2.end() - 1, 400, 99);
 
-    for (int x : v2)
+    auto it = find(v2.begin(), v2.end(), 5);
+    cout << *it << endl;
+
+    if (it == v2.end())
     {
-        cout << x << " ";
+        cout << "Not found";
     }
+    else
+    {
+        cout << "Found";
+    }
+
+    // for (int x : v2)
+    // {
+    //     cout << x << " ";
+    // }
 
     return 0;
 }
