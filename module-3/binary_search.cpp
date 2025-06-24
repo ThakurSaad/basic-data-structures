41623992dff0e3f0cbc8b41549d2981d24fc9181
ThakurSaad <thakursaad613@gmail.com>
2025-06-24 08:23:41 +0600

    binary search

---
+++ module-3/binary_search.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int x;
+    cin >> x;
+
+    int l = a[0],
+        r = a[n - 1],
+        flag = 0;
+
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+
+        if (a[mid] == x)
+        {
+            flag = 1;
+            break;
+        }
+        else if (a[mid] > x)
+        {
+            r = mid - 1;
+        }
+        else if (a[mid] < x)
+        {
+            l = mid + 1;
+        }
+    }
+
+    if (flag)
+    {
+        cout << "found";
+    }
+    else
+    {
+        cout << "not found";
+    }
+
+    return 0;
+}
