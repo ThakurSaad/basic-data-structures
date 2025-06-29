e12b9ab45257b738d6ddd3f0480cdcb631071207
ThakurSaad <thakursaad613@gmail.com>
2025-06-29 17:10:23 +0600

    creating node with constructor

---
+++ module-5/creating_node_with_constructor.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+
+    Node a(10),
+        b(200),
+        c(30);
+
+    a.next = &b;
+    b.next = &c;
+
+    cout << a.val << endl
+         << a.next->val << endl
+         << a.next->next->val;
+
+    return 0;
+}
