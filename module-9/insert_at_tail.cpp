fe0e6a5da9fc31bb198ff136e9c2dd15dc08208d
ThakurSaad <thakursaad613@gmail.com>
2025-07-07 18:27:09 +0600

    insert at head and tail

--- module-9/insert_at_tail.cpp
+++ module-9/insert_at_tail.cpp
@@ -5,14 +5,14 @@ class Node
 {
 public:
     int val;
-    Node *prev;
     Node *next;
+    Node *prev;
 
     Node(int val)
     {
         this->val = val;
-        this->prev = NULL;
         this->next = NULL;
+        this->prev = NULL;
     }
 };
 
@@ -25,16 +25,7 @@ void print_forward(Node *head)
     cout << endl;
 }
 
-void print_backward(Node *tail)
-{
-    for (Node *i = tail; i != NULL; i = i->prev)
-    {
-        cout << i->val << " ";
-    }
-    cout << endl;
-}
-
-void insert_at_head(Node *&head, Node *&tail, int val)
+void insert_at_tail(Node *&head, Node *&tail, int val)
 {
     Node *new_node = new Node(val);
 
@@ -45,27 +36,27 @@ void insert_at_head(Node *&head, Node *&tail, int val)
         return;
     }
 
-    head->prev = new_node;
-    new_node->next = head;
-    head = new_node;
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
 }
 
 int main()
 {
+    // Node *head = NULL;
+    // Node *tail = NULL;
     Node *head = new Node(10);
     Node *a = new Node(20);
     Node *tail = new Node(30);
 
     head->next = a;
-
     a->prev = head;
     a->next = tail;
-
     tail->prev = a;
 
-    insert_at_head(head, tail, 100);
-    insert_at_head(head, tail, 111);
+    insert_at_tail(head, tail, 200);
+    insert_at_tail(head, tail, 400);
     print_forward(head);
-    print_backward(tail);
+
     return 0;
 }
