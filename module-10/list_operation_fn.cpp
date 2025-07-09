c325250f036a2b9e79dc80e48854364bf6545ed6
ThakurSaad <thakursaad613@gmail.com>
2025-07-09 20:42:42 +0600

    list operation

---
+++ module-10/list_operation_fn.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> l = {10, 23, 432, 5426, 23, 23, 43, 545, 23, 45};
+
+    // l.remove(23);
+    l.sort(greater<int>()); // descending
+    l.unique();
+
+    for (int val : l)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
+}
