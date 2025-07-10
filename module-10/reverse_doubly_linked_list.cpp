eaa077da43699f4913c89f0380376e60923398ad
ThakurSaad <thakursaad613@gmail.com>
2025-07-10 15:05:39 +0600

    reverse doubly linked list

---
+++ module-10/reverse_doubly_linked_list.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void doubly_insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+}
+
+void print_linked_list(Node *&head)
+{
+    Node *tmp = head;
+
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+
+void reverse_doubly(Node *head, Node *tail, Node *tmp)
+{
+
+    for (Node *i = head, *j = tail; i != j && i->prev != j; i = i->next, j = j->prev)
+    {
+        swap(i->val, j->val);
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+
+        if (val == -1)
+        {
+            break;
+        }
+        doubly_insert_at_tail(head, tail, val);
+    }
+
+    reverse_doubly(head, tail, head);
+    print_linked_list(head);
+
+    return 0;
+}
