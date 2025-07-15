0b439e97fbd668ed0afea5f094714d2a32585bb4
ThakurSaad <thakursaad613@gmail.com>
2025-07-15 15:14:55 +0600

    reverse solved

---
+++ module-10.5/practice-2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_list(list<int> l)
+{
+    for (auto val : l)
+    {
+        cout << val << " ";
+    }
+}
+
+int main()
+{
+
+    list<int> l;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        l.push_back(val);
+    }
+    l.reverse();
+    print_list(l);
+
+    return 0;
+}
