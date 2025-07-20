02be37b6f7814488e808cc63761678afb7f0d0f8
ThakurSaad <thakursaad613@gmail.com>
2025-07-20 20:30:47 +0600

    mystack building

--- module-13/implement_stack_using_arr.cpp
+++ module-13/implement_stack_using_arr.cpp
@@ -1,10 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class myStack
+{
+public:
+    vector<int> v;
+
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        return v.empty();
+    }
+};
+
 int main()
 {
-    string s;
-    cin >> s;
-    cout << s;
+    myStack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    cout << st.size() << endl;
+    cout << st.top() << endl;
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    cout << st.top() << endl;
+    cout << st.size() << endl;
+    cout << st.empty() << endl;
+    cout << st.top() << endl;
+
     return 0;
 }
