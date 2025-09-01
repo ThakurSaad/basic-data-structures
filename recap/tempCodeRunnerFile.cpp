9bf2b8f725cfa359b349bb5d8c9f48db2975b8c1
ThakurSaad <thakursaad613@gmail.com>
2025-09-01 19:40:38 +0600

    input_tree

---
+++ recap/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        cout << f->val << " ";
+
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
