708f6ef0648264b7d6abd4fd64c1f97ad0bd372e
ThakurSaad <thakursaad613@gmail.com>
2025-09-05 22:51:47 +0600

    5 problems solved on binaray tree

---
+++ module-19/node_level.cpp
@@ -0,0 +1,30 @@
+// https://www.naukri.com/code360/problems/node-level_920383
+
+/*
+
+int nodeLevel(TreeNode<int>* root, int searchedValue)
+{
+    queue<pair<TreeNode<int>*, int>> q;
+
+    if (root)
+        q.push({root, 1});
+
+    while (!q.empty()) {
+        pair<TreeNode<int>*, int> parent = q.front();
+        q.pop();
+
+        TreeNode<int>* node = parent.first;
+        int level = parent.second;
+
+        if (node->val == searchedValue)
+            return level;
+
+        if (node->left)
+            q.push({node->left, level+1});
+        if (node -> right)
+            q.push({node->right, level+1});
+
+    }
+}
+
+*/
---
+++ module-19/pair.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    pair<int, int> p;
+    p = make_pair(2, 3);
+    cout << p.first << endl;
+    cout << p.second << endl;
+
+    pair<string, int> p1;
+    p1 = {"Hello", 4};
+    cout << p1.first << endl;
+    cout << p1.second << endl;
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].first << " " << v[i].second << endl;
+    }
+
+    return 0;
+}
